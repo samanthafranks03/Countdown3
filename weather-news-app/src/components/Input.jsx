8e8470ea83d171e1201c2867ed0482c84b7162b8
samanthafranks03 <rdu5qu@virginia.edu>
2025-04-12 20:05:43 +0200

    Added button to get the latitude, longitude, and weather forcast for the user

--- weather-news-app/src/components/Input.jsx
+++ weather-news-app/src/components/Input.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import TextField from '@mui/material/TextField';
+import {TextField, Button} from '@mui/material';
 
 function Input(){
     //Allow user to type in address, city name, state, and zipcode
     return (
         <>
-            <TextField id="address-label" label="Address" variant="outlined"/>
-            <TextField id="city-label" label="City" variant="outlined"/>
-            <TextField id="state-label" label="State" variant="outlined"/>
-            <TextField id="zipcode-label" label="Zipcode" variant="outlined"/>
+            <h2>Insert your location here</h2>
+            <TextField className="input" label="Address" variant="outlined"/>
+            <TextField className="input" label="City" variant="outlined"/>
+            <TextField className="input" label="State" variant="outlined"/>
+            <TextField className="input" label="Zipcode" variant="outlined"/>
+            <Button variant="contained">Get Weather Forecast</Button>
         </>
 
     );
